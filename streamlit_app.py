48d7b027de5e47c2ffaf3027a250957e4d58fe75
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-05-05 21:57:11 +0900

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ with m2:
     cellStyle = JsCode(
         r"""
         function(cellClassParams) {
-             if (cellClassParams.data.RecommendedSetting === null ) {
+             if (cellClassParams.data.Recommended === null ) {
                 return {'background-color': `gray`}
              } else {
                 return {'background-color': 'yellow'}
